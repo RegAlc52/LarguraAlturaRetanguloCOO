ce0935ed3adb817af49339deb5dc65d95f6c4581
Regiane_de_Alcântara <any_any_2011@hotmail.com>
2024-03-03 10:14:12 -0300

    InicioProjeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LarguraAlturaRetanguloCOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
