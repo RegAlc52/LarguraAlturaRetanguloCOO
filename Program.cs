5bc1dd48d52a9c1e7b58afcbde4b03706d0faee8
Regiane_de_Alcântara <any_any_2011@hotmail.com>
2024-03-03 10:32:50 -0300

    ProjetoAjustes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿
+//Fazer um programa para ler os valores 
+//largura e altura de um retângulo. 
+//Mostrar em tela o valor de 
+//sua área, perímetro e diagonal.
+//A Classe para esta atividade deve 
+//conter os atributos Largura e Altura 
+//os Métodossing System; using System;
+//CalcularArea() : double, 
+//CalcularPerimetro: double
+//CalcularDiagonal(): double
+//Com Orintação a Objetos
 
-namespace LarguraAlturaRetanguloCOO
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+
+//Strings
+//largura do retângulo
+//altura do retângulo
+//Área = double
+//Perímetro
+//Diagonal
+
+//Variaveis
+//largura = double
+//altura = double
+//retangulo = retangulo
